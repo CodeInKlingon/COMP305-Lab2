200077acce6cb8821d8d7f110bf2cdc5bdf12577
Daniel Stokes <danielphilipstokes@gmail.com>
2017-09-13 14:42:10 -0400

    Sprite import and script creation

---
+++ Lab2/Assets/SpriteRevealer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRevealer : MonoBehaviour {
+
+    Sprite pixel;
+    public Sprite real;
+
+    // Use this for initialization
+    protected void OnMouseEnter() {
+        pixel = GetComponent<SpriteRenderer>().sprite;
+        GetComponent<SpriteRenderer>().sprite = real;
+    }
+
+    protected void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().sprite = pixel;
+    }
+}
---
+++ Lab2/Assets/SpriteSwapper.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSwapper : MonoBehaviour {
+
+    public Sprite[] sprites;
+
+    public GameObject sprite;
+    public bool reverse;
+
+
+    // Use this for initialization
+    protected void OnMouseDown()
+    {
+        print("mouse down");
+        string currentSprite = sprite.GetComponent<SpriteRenderer>().sprite.name.ToString();
+        if (!reverse)
+        {
+            switch (currentSprite)
+            {
+                case "Daniel1":
+                    sprite.GetComponent<SpriteRenderer>().sprite = sprites[1];
+                    break;
+                case "Daniel2":
+                    sprite.GetComponent<SpriteRenderer>().sprite = sprites[2];
+                    break;
+                case "Daniel3":
+                    sprite.GetComponent<SpriteRenderer>().sprite = sprites[0];
+                    break;
+            }
+        }
+        else
+        {
+            switch (currentSprite)
+            {
+                case "Daniel1":
+                    sprite.GetComponent<SpriteRenderer>().sprite = sprites[2];
+                    break;
+                case "Daniel2":
+                    sprite.GetComponent<SpriteRenderer>().sprite = sprites[0];
+                    break;
+                case "Daniel3":
+                    sprite.GetComponent<SpriteRenderer>().sprite = sprites[1];
+                    break;
+            }
+        }
+    }
+}
